8be27abaf50c12483f80346fa5cff248331550f8
Micah Longmire <mlmicahlongmire@gmail.com>
2023-01-06 13:49:38 -0700

    yadayada

---
+++ Project_Default/Git-Origin.sh
@@ -0,0 +1,6 @@
+# Remove all existing values for remote.origin.url
+git config --unset-all remote.origin.url
+
+# Set a new value for remote.origin.url
+git config remote.origin.url "git@github.com:bobbyhiddn/{Name_Of_Project}.git"
+
